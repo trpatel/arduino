70a85625137c714f57d9c6f9b5605fbb1cee9ec8
Tej Patel <trpatel@indiana.edu>
2016-01-28 23:54:52 -0500

    example

---
+++ sketch1.ino
@@ -0,0 +1,115 @@
+/*
+ 
+  This example code is in the public domain.
+ */
+ 
+// Pin 13 has a Buzzer connected on most Arduino boards.
+// give it a name:
+int buzzer = 13;
+
+// the setup routine runs once when you press reset:
+void setup() {                
+  // initialize the digital pin as an output.
+  pinMode(buzzer, OUTPUT);    
+}
+
+// the loop routine runs over and over again forever:
+void loop() {
+
+  //SOS
+  //S
+  digitalWrite(buzzer, HIGH);
+  delay(250);
+  digitalWrite(buzzer, LOW);
+  delay(250);
+  digitalWrite(buzzer, HIGH);
+  delay(250);
+  digitalWrite(buzzer, LOW);
+  delay(250);
+  digitalWrite(buzzer, HIGH);
+  delay(250);
+  digitalWrite(buzzer, LOW);
+  delay(750);
+  //O
+  digitalWrite(buzzer, HIGH);
+  delay(750);
+  digitalWrite(buzzer, LOW);
+  delay(250);
+  digitalWrite(buzzer, HIGH);
+  delay(750);
+  digitalWrite(buzzer, LOW);
+  delay(250);
+  digitalWrite(buzzer, HIGH);
+  delay(750);
+  digitalWrite(buzzer, LOW);
+  delay(750);
+  //S
+  digitalWrite(buzzer, HIGH);
+  delay(250);
+  digitalWrite(buzzer, LOW);
+  delay(250);
+  digitalWrite(buzzer, HIGH);
+  delay(250);
+  digitalWrite(buzzer, LOW);
+  delay(250);
+  digitalWrite(buzzer, HIGH);
+  delay(250);
+  digitalWrite(buzzer, LOW);
+  delay(1750);
+
+
+  //Last name: PATEL
+  //  P
+  digitalWrite(buzzer, HIGH);
+  delay(250);
+  digitalWrite(buzzer, LOW);
+  delay(250);
+  digitalWrite(buzzer, HIGH);
+  delay(750);
+  digitalWrite(buzzer, LOW);
+  delay(250);
+  digitalWrite(buzzer, HIGH);
+  delay(750);
+  digitalWrite(buzzer, LOW);
+  delay(250);
+  digitalWrite(buzzer, HIGH);
+  delay(250);
+  digitalWrite(buzzer, LOW);
+  delay(750);
+  //   A
+  digitalWrite(buzzer, HIGH);
+  delay(250);
+  digitalWrite(buzzer, LOW);
+  delay(250);
+  digitalWrite(buzzer, HIGH);
+  delay(750);
+  digitalWrite(buzzer, LOW);
+  delay(750);
+  // T
+  digitalWrite(buzzer, HIGH);
+  delay(750);
+  digitalWrite(buzzer, LOW);
+  delay(750);
+  // E
+  digitalWrite(buzzer, HIGH);
+  delay(250);
+  digitalWrite(buzzer, LOW);
+  delay(750);
+  // L
+  digitalWrite(buzzer, HIGH);
+  delay(250);
+  digitalWrite(buzzer, LOW);
+  delay(250);
+  digitalWrite(buzzer, HIGH);
+  delay(750);
+  digitalWrite(buzzer, LOW);
+  delay(250);
+  digitalWrite(buzzer, HIGH);
+  delay(250);
+  digitalWrite(buzzer, LOW);
+  delay(250);
+  digitalWrite(buzzer, HIGH);
+  delay(250);
+  digitalWrite(buzzer, LOW);
+  delay(1750);
+}
